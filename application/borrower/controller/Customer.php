3ed45c1dad0910c6a58e6fd8a00eff17ae7af9df
yangfan <494137348@qq.com>
2018-04-26 09:27:50 +0800

    客户服务

---
+++ application/borrower/controller/Customer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/25
+ * Time: 18:32
+ */
+
+namespace app\borrower\controller;
+
+
+use app\user\controller\Base;
+use think\Controller;
+
+use app\common\model\Borrower;
+use app\common\selfConfig\StatusCode;
+use http\Env\Response;
+use think\Session;
+use think\Request;
+
+
+class Customer extends Base
+{
+    public function index(){//列表信息
+        $data = Borrower::selectEntity();
+        $this->assign('data',$data);
+        return $this->fetch('user/index',$data);
+    }
+    public function single(){//修改
+        $id = Request::instance()->param('id');
+        var_dump($id);
+        exit;
+        //$data = Borrower::findEntity()
+    }
+    public function server(){//客户服务
+
+
+    }
+
+}
