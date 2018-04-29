e8237233c1a697ddf9928bebbc6c464c95287ce1
yangfan <494137348@qq.com>
2018-04-29 11:31:58 +0800

    yfan

---
+++ application/home/controller/Index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/29
+ * Time: 10:11
+ */
+namespace app\home\controller;
+use think\Controller;
+class Index extends Controller
+{
+    public function index(){//首页
+        return $this->fetch("index/index");
+    }
+    public function service(){//数据服务
+        return $this->fetch('index/service');
+    }
+    public function news(){//新闻咨询
+        return $this->fetch('index/news');//新闻咨询
+    }
+    public function about(){//关于我们
+        return $this->fetch('index/about-us');
+    }
+    public function cooperation(){//合作入住
+        return $this->fetch('index/cooperation');
+    }
+
+}
